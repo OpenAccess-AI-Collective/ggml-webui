3143561deed644482b91746652f7169d0c30773f
Wing Lian <wing.lian@gmail.com>
2023-05-15 02:18:54 -0400

    update instruct to use gpu, add verbose description

--- instruct.py
+++ instruct.py
@@ -12,15 +12,20 @@ fp = hf_hub_download(
 llm = Llama(model_path=fp)
 
 def generate_text(input_text):
-    output = llm(f"### Instruction: {input_text}\n\n### Response: ", max_tokens=256, stop=["</s>", "<unk>", "### Instruction:"], echo=True)
+    output = llm(f"### Instruction: {input_text}\n\n### Response: ", stop=["</s>", "<unk>", "### Instruction:"], echo=True, **config['chat'])
     return output['choices'][0]['text']
 
 input_text = gr.inputs.Textbox(lines= 10, label="Enter your input text")
 output_text = gr.outputs.Textbox(label="Output text")
 
-description = f"""llama.cpp implementation in python [https://github.com/abetlen/llama-cpp-python]
-
-This is the {config["repo"]}/{config["file"]} model.
+description = f"""
+### brought to you by OpenAccess AI Collective
+- This is the [{config["repo"]}](https://huggingface.co/{config["repo"]}) model file [{config["file"]}](https://huggingface.co/{config["repo"]}/blob/main/{config["file"]})
+- This Space uses GGML with GPU support, so it can quickly run larger models on smaller GPUs & VRAM.
+- This is running on a smaller, shared GPU, so it may take a few seconds to respond. 
+- [Duplicate the Space](https://huggingface.co/spaces/openaccess-ai-collective/ggml-ui?duplicate=true) to skip the queue and run in a private space or to use your own GGML models.
+- When using your own models, simply update the [config.yml](https://huggingface.co/spaces/openaccess-ai-collective/ggml-ui/blob/main/config.yml)")
+- Contribute at [https://github.com/OpenAccess-AI-Collective/ggml-webui](https://github.com/OpenAccess-AI-Collective/ggml-webui)
 """
 
 gr.Interface(
